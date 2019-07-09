b6a1c2c9ca44d88e8d6753cefd484b5d295e6f89
chengshq <csq1124@gmail.com>
2019-07-09 19:46:51 +0800

    fix Dock 唤醒

--- TransTimestamp/AppDelegate.swift
+++ TransTimestamp/AppDelegate.swift
@@ -11,16 +11,24 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var mainWindow: NSWindow!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        mainWindow = NSApplication.shared.windows[0]
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows
+        flag: Bool) -> Bool {
+        if !flag{
+            mainWindow.makeKeyAndOrderFront(nil)
+        }
+        return true
+    }
+    
 }
 
